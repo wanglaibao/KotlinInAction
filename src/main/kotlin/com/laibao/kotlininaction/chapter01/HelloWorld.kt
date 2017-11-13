6ef762e9afaf372346c2684ff6029c883d0ade29
jinge <jinge@wacai.com>
2017-11-13 20:08:10 +0800

    add HelloWorld.kt file info

---
+++ src/main/kotlin/com/laibao/kotlininaction/chapter01/HelloWorld.kt
@@ -0,0 +1,11 @@
+package com.laibao.kotlininaction.chapter01
+
+/**
+ * @author laibao wang
+ * @date 2017-11-13
+ * @version 1.0
+ *
+ */
+fun main(args: Array<String>) {
+    println("Hello KotlinInAction, Welcome Here!")
+}
