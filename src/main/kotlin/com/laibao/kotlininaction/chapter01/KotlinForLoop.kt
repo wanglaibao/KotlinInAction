2796a276c9591b0033de416ba436dc5150313c88
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-03-09 22:56:28 +0800

    add KotlinForLoop.kt file info

---
+++ src/main/kotlin/com/laibao/kotlininaction/chapter01/KotlinForLoop.kt
@@ -0,0 +1,38 @@
+package com.laibao.kotlininaction.chapter01
+
+/**
+ * @author laibao wang
+ * @date 2018-03-09
+ * @version 1.0
+ */
+
+fun main(args: Array<String>) {
+    //val daysOfWeek : List<String> = listOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+    val daysOfWeek = listOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+    //pure for loop
+    for(day in daysOfWeek) {
+        println(day)
+    }
+
+    //pairs
+    for((index,day) in daysOfWeek.withIndex()) {
+        println("$index :$day")
+    }
+
+    //forEach Loop
+    daysOfWeek.forEach{day -> println(day)}
+
+    // loop in range
+    val range:IntRange = 1 .. 100
+    for(element in range){
+        println(element)
+    }
+
+    val range1:IntRange = 1 until 100
+    for(element in range1){
+        println(element)
+    }
+
+    // repeat statement
+    repeat(1000) {println("金戈")}
+}
