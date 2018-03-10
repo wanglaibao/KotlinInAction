f9cff46ca33fa794c9e437f2ae4206836175591a
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-03-10 11:16:34 +0800

    add WeekDay enum class file info

---
+++ src/main/kotlin/com/laibao/kotlininaction/chapter01/WeekDay.kt
@@ -0,0 +1,16 @@
+package com.laibao.kotlininaction.chapter01
+
+/**
+ *@author laibao wang
+ * @date 2018-03-10
+ * @version 1.0
+ */
+enum class WeekDay {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
